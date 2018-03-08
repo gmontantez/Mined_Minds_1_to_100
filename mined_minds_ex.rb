13782d9dbf9cddaf2bd55e0c3f1176f112d4a861
gmontantez <gmontantez7@gmail.com>
2018-03-08 10:10:21 -0500

    Mined Minds 1-100

---
+++ mined_minds_ex.rb
@@ -0,0 +1,9 @@
+def empty_array
+	[]
+	
+end
+
+def full_array
+	numbers = [1..100]
+
+end
---
+++ mined_minds_test_ex.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require_relative "mined_minds_ex.rb"
+ 
+class Mined_minds < Minitest::Test
+	def test_boolean
+		assert_equal(true,true)
+	end
+
+	def test_empty_array
+		assert_equal([], empty_array)
+	end
+
+	def test_1_to_100
+		assert_equal([1..100], full_array)
+	end
+
+	def test_3_to_mined
+		assert_equal("mined", full_array)
+	end
+	
+end
